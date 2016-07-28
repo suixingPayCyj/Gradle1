fe39f7a7c52eeca84cdcb6fb177960d5a2c8798a
suixingPayCyj <xi_yj@suixingpay.com>
2016-07-28 13:41:07 +0800

    首次提交

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package main.java.hello;
+
+import org.joda.time.LocalTime;
+
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		 Greeter greeter = new Greeter();
+		 LocalTime currentTime = new LocalTime();
+		 System.out.println("The current local time is: " + currentTime);
+		 System.out.println(greeter.sayHello());
+	}
+}
+	
